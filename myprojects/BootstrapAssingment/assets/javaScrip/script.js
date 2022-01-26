e8d8f90ab828e367fc5f59330400b171b4421712
Ravidu lakshitha <ravidulakshitha360@gmail.com>
2022-01-26 12:03:41 +0530

    js

---
+++ myprojects/BootstrapAssingment/assets/javaScrip/script.js
@@ -0,0 +1,30 @@
+document.getElementById("btn-placeOrder").addEventListener("click",function(){
+    document.getElementsByClassName("placeOrder-area")[0].style.display="block"
+    document.getElementsByClassName("customer-area")[0].style.display="none"
+    document.getElementsByClassName("item-area")[0].style.display="none"
+    document.getElementsByClassName("home")[0].style.display="block"
+
+})
+
+document.getElementById("btn-ManageCustomer").addEventListener("click",function(){
+    document.getElementsByClassName("customer-area")[0].style.display="block"
+    document.getElementsByClassName("placeOrder-area")[0].style.display="none"
+    document.getElementsByClassName("item-area")[0].style.display="none"
+    document.getElementsByClassName("home")[0].style.display="block"
+
+})
+
+document.getElementById("btn-ManageStore").addEventListener("click",function(){
+    document.getElementsByClassName("item-area")[0].style.display="block"
+    document.getElementsByClassName("placeOrder-area")[0].style.display="none"
+    document.getElementsByClassName("customer-area")[0].style.display="none"
+    document.getElementsByClassName("home")[0].style.display="block"
+
+})
+document.getElementById("btn-Home").addEventListener("click",function(){
+    document.getElementsByClassName("home")[0].style.display="block"
+    document.getElementsByClassName("item-area")[0].style.display="none"
+    document.getElementsByClassName("placeOrder-area")[0].style.display="none"
+    document.getElementsByClassName("customer-area")[0].style.display="none"
+
+})
