7f917c6f663d1bf80a049db8081ecd0fa533ca5c
Ravidu lakshitha <ravidulakshitha360@gmail.com>
2022-01-26 21:13:11 +0530

    Content update

--- myprojects/BootstrapAssingment/assets/javaScrip/script.js
+++ myprojects/BootstrapAssingment/assets/javaScrip/script.js
@@ -2,7 +2,7 @@ document.getElementById("btn-placeOrder").addEventListener("click",function(){
     document.getElementsByClassName("placeOrder-area")[0].style.display="block"
     document.getElementsByClassName("customer-area")[0].style.display="none"
     document.getElementsByClassName("item-area")[0].style.display="none"
-    document.getElementsByClassName("home")[0].style.display="block"
+    document.getElementsByClassName("home")[0].style.display="none"
 
 })
 
@@ -10,7 +10,7 @@ document.getElementById("btn-ManageCustomer").addEventListener("click",function(
     document.getElementsByClassName("customer-area")[0].style.display="block"
     document.getElementsByClassName("placeOrder-area")[0].style.display="none"
     document.getElementsByClassName("item-area")[0].style.display="none"
-    document.getElementsByClassName("home")[0].style.display="block"
+    document.getElementsByClassName("home")[0].style.display="none"
 
 })
 
@@ -18,7 +18,7 @@ document.getElementById("btn-ManageStore").addEventListener("click",function(){
     document.getElementsByClassName("item-area")[0].style.display="block"
     document.getElementsByClassName("placeOrder-area")[0].style.display="none"
     document.getElementsByClassName("customer-area")[0].style.display="none"
-    document.getElementsByClassName("home")[0].style.display="block"
+    document.getElementsByClassName("home")[0].style.display="none"
 
 })
 document.getElementById("btn-Home").addEventListener("click",function(){
